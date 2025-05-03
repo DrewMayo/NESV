2ed31cbf82b2868e8c73be36b45cff170d99ca0b
msorial2 <143022385+msorial2@users.noreply.github.com>
2025-05-03 01:24:02 -0500

    Update controller.sv

--- nes.srcs/sources_1/new/controller.sv
+++ nes.srcs/sources_1/new/controller.sv
@@ -37,7 +37,6 @@ module controller(
         output logic uart_rtl_0_txd,
         
         //CPU
-        input logic [7:0] cpu_data_in,
         input logic [15:0] cpu_addr,
         output logic [7:0] controller_out,
         input logic R_W_n
@@ -46,7 +45,7 @@ module controller(
     logic [31:0] keycode0_gpio, keycode1_gpio;
     
     mb_block mb_block_i (
-        .clk_100MHz(Clk),
+        .clk_25MHz(Clk),
         .gpio_usb_int_tri_i(gpio_usb_int_tri_i),
         .gpio_usb_keycode_0_tri_o(keycode0_gpio),
         .gpio_usb_keycode_1_tri_o(keycode1_gpio),
@@ -66,63 +65,45 @@ module controller(
     logic read_en2;
     logic write_en;
     
-    assign read_en1 = (cpu_addr == 16'h4016 && R_W_n);
-    assign read_en2 = (cpu_addr == 16'h4017 && R_W_n);
-    
-    assign write_en = (cpu_addr == 16'h4016 && R_W_n == 1'b0 && cpu_data_in[0]);
-    
+    logic [7:0] keys [7:0];
+
     always_comb begin
-        for ( int i = 31; i > 0; i -= 8) begin
-            //Gamepad 1
-            if (keycode0_gpio[i-:8] == 8'h1A || keycode1_gpio[i-:8] == 8'h1A) begin //W - Up
-                gamepad1_buffer[3] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h04 || keycode1_gpio[i-:8] == 8'h04) begin //A - Left
-                gamepad1_buffer[1] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h16 || keycode1_gpio[i-:8] == 8'h16) begin //S - Down
-                gamepad1_buffer[2] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h07 || keycode1_gpio[i-:8] == 8'h07) begin //D - Right
-                gamepad1_buffer[0] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h06 || keycode1_gpio[i-:8] == 8'h06) begin //C - A
-                gamepad1_buffer[7] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h19 || keycode1_gpio[i-:8] == 8'h19) begin //V - B
-                gamepad1_buffer[6] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h14 || keycode1_gpio[i-:8] == 8'h14) begin //Q - Select
-                gamepad1_buffer[5] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h08 || keycode1_gpio[i-:8] == 8'h08) begin //E - Start
-                gamepad1_buffer[4] = 1'b1;
-            end
-            //Gamepad 2
-            if (keycode0_gpio[i-:8] == 8'h52 || keycode1_gpio[i-:8] == 8'h52) begin //Arrow Up - Up
-                gamepad2_buffer[3] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h50 || keycode1_gpio[i-:8] == 8'h50) begin //Arrow Left - Left
-                gamepad2_buffer[1] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h51 || keycode1_gpio[i-:8] == 8'h51) begin //Arrow Down - Down
-                gamepad2_buffer[2] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h4F || keycode1_gpio[i-:8] == 8'h4F) begin //Arrow Right - Right
-                gamepad2_buffer[0] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h36 || keycode1_gpio[i-:8] == 8'h36) begin //< - A
-                gamepad2_buffer[7] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h37 || keycode1_gpio[i-:8] == 8'h37) begin //> - B
-                gamepad2_buffer[6] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h33 || keycode1_gpio[i-:8] == 8'h33) begin //; - Select
-                gamepad2_buffer[5] = 1'b1;
-            end
-            if (keycode0_gpio[i-:8] == 8'h34 || keycode1_gpio[i-:8] == 8'h34) begin //' - Start
-                gamepad2_buffer[4] = 1'b1;
-            end     
+        // Slice keycode GPIOs into 8 keycodes total (4 from each)
+        keys[0] = keycode0_gpio[ 7: 0];
+        keys[1] = keycode0_gpio[15: 8];
+        keys[2] = keycode0_gpio[23:16];
+        keys[3] = keycode0_gpio[31:24];
+        keys[4] = keycode1_gpio[ 7: 0];
+        keys[5] = keycode1_gpio[15: 8];
+        keys[6] = keycode1_gpio[23:16];
+        keys[7] = keycode1_gpio[31:24];
+
+        gamepad1_buffer = 8'b0;
+        gamepad2_buffer = 8'b0;
+
+        for (int i = 0; i < 8; i++) begin
+            case (keys[i])
+                // Gamepad 1 mappings
+                8'h1A: gamepad1_buffer[3] = 1; // W - Up
+                8'h04: gamepad1_buffer[1] = 1; // A - Left
+                8'h16: gamepad1_buffer[2] = 1; // S - Down
+                8'h07: gamepad1_buffer[0] = 1; // D - Right
+                8'h06: gamepad1_buffer[7] = 1; // C - A
+                8'h19: gamepad1_buffer[6] = 1; // V - B
+                8'h14: gamepad1_buffer[5] = 1; // Q - Select
+                8'h08: gamepad1_buffer[4] = 1; // E - Start
+
+                // Gamepad 2 mappings
+                8'h52: gamepad2_buffer[3] = 1; // Arrow Up
+                8'h50: gamepad2_buffer[1] = 1; // Arrow Left
+                8'h51: gamepad2_buffer[2] = 1; // Arrow Down
+                8'h4F: gamepad2_buffer[0] = 1; // Arrow Right
+                8'h36: gamepad2_buffer[7] = 1; // < - A
+                8'h37: gamepad2_buffer[6] = 1; // > - B
+                8'h33: gamepad2_buffer[5] = 1; // ; - Select
+                8'h34: gamepad2_buffer[4] = 1; // ' - Start
+                default: /* do nothing */;
+            endcase
         end
     end
     
@@ -130,25 +111,30 @@ module controller(
     logic gamepad2_out;
     
     always_comb begin 
-        controller_out[7:1] = 7'b0000000;
         if (cpu_addr == 16'h4016) begin
-            controller_out[0] = gamepad1_out;
+            controller_out = {7'b0, gamepad1_out};
         end else if (cpu_addr == 16'h4017) begin
-            controller_out[0] = gamepad2_out;
+            controller_out = {7'b0, gamepad2_out};
+        end else begin
+            controller_out = 8'b0;
         end
     end
     
     logic gamepad1_CLK;
     logic gamepad2_CLK;
     
-    assign gamepad1_CLK = ~(R_W_n & cpu_addr == 16'h4016);
-    assign gamepad2_CLK = ~(R_W_n & cpu_addr == 16'h4017);
+    assign write_en = (cpu_addr == 16'h4016 && ~R_W_n);
+    
+    assign gamepad1_CLK = ~(R_W_n && cpu_addr == 16'h4016);
+    assign gamepad2_CLK = ~(R_W_n && cpu_addr == 16'h4017);
     
     shift_reg gamepad1(
         .parallel_in(gamepad1_buffer),
         .serial_in(1'b0),
         .ps(write_en),
-        .Clk(gamepad1_CLK),
+        .shift_en(gamepad1_CLK),
+        .Clk(Clk),
+        .reset(reset_rtl_0),
         
         .serial_out(gamepad1_out)
     );
@@ -157,7 +143,9 @@ module controller(
         .parallel_in(gamepad2_buffer),
         .serial_in(1'b0),
         .ps(write_en),
-        .Clk(gamepad2_CLK),
+        .shift_en(gamepad2_CLK),
+        .Clk(Clk),
+        .reset(reset_rtl_0),
         
         .serial_out(gamepad2_out)
     );
@@ -168,28 +156,33 @@ endmodule
 
 module shift_reg(
     input logic Clk,
+    input logic reset,
     input logic [7:0] parallel_in,
     input logic ps, //parallel or serial (parallel load signal)
+    input logic shift_en,
     input logic serial_in,
     
     output logic serial_out
 );
 
     logic [7:0] Q;
-    assign serial_out = Q[7];
+    logic last_shift;
     
-    always_ff@(posedge Clk, posedge ps) begin
-        if (ps == 1'b1) begin
-            Q[7:0] <= parallel_in[7:0];
-        end else if (ps == 1'b0) begin
-            Q[7] <= Q[6];
-            Q[6] <= Q[5];
-            Q[5] <= Q[4];
-            Q[4] <= Q[3];
-            Q[3] <= Q[2];
-            Q[2] <= Q[1];
-            Q[1] <= Q[0];
-            Q[0] <= serial_in;
+    always_ff@(posedge Clk) begin
+        if (~reset) begin
+            last_shift <= shift_en;
+            serial_out <= Q[0];
+            
+            if (~last_shift && shift_en) begin
+                Q <= {1'b1, Q[7:1]};
+            end else if (last_shift && ~shift_en) begin
+                Q <= Q;
+            end else if (ps) begin
+                Q <= parallel_in;
+            end
+        end else begin
+            Q <= 8'b0;
+            last_shift <= 1'b0;
         end
     end
 
